45948c4787000dab329798f04366e964edafc5a8
Manuel Schmitz <manuel.schmitz@gmx.net>
2022-04-19 18:50:09 +0200

    Add StyleGAN Generator

--- networks/stylegan.py
+++ networks/stylegan.py
@@ -1,38 +1,120 @@
+import random
+
 import numpy as np
-from torch import Tensor, nn
+import torch
+from torch import nn
 
-from networks.utils.layers import PixelwiseNorm, ScaledDense
+from networks.modules.stylegan import (
+    GenGeneralConvBlock,
+    GenInitialBlock,
+    StyleGANMappingNetwork,
+    Truncation,
+)
+from networks.utils import calc_channels_at_stage
+from networks.utils.layers import ScaledConv2d
 
 
-class StyleGANMappingNetwork(nn.Module):
-    def __init__(self, latent_size: int, mapping_layers: int):
-        """
-        StyleGan Mapping Network as described in the paper `A Style-Based
-        Generator Architecture for Generative Adversarial Networks`
-
-        Args:
-            latent_size: Size of the latent space
-            mapping_layers: Number of layers in the mapping network
-
-        References:
-            https://arxiv.org/pdf/1812.04948.pdf
-        """
+class GSynthesis(nn.Module):
+    def __init__(
+        self,
+        n_blocks: int = 10,
+        latent_size: int = 512,
+        n_channels: int = 3,
+    ):
         super().__init__()
-        self.lrelu = nn.LeakyReLU(0.2)
-        self.mapping_network = nn.ModuleList([PixelwiseNorm()])
-        for _ in range(mapping_layers):
-            self.mapping_network.append(
-                ScaledDense(latent_size, latent_size, gain=np.sqrt(2))
+
+        self.n_blocks = n_blocks
+        self.latent_size = latent_size
+        self.n_channels = n_channels
+
+        self.layers = nn.ModuleList()
+        self.layers.append(
+            GenInitialBlock(
+                latent_size,
+                calc_channels_at_stage(0),
             )
-            self.mapping_network.append(self.lrelu)
+        )
 
-    def forward(self, x: Tensor):
-        """
-        Passes the input tensor through the mapping network.
+        for block in range(0, n_blocks):
+            self.layers.append(
+                GenGeneralConvBlock(
+                    calc_channels_at_stage(block),
+                    calc_channels_at_stage(block + 1),
+                    dlatent_size=latent_size,
+                )
+            )
 
-        Args:
-            x: Latent space vector
-        """
-        for layer in self.mapping_network:
-            x = layer(x)
-        return x
+        self.rgb_converters = nn.ModuleList(
+            [
+                ScaledConv2d(
+                    calc_channels_at_stage(stage),
+                    n_channels,
+                    kernel_size=(1, 1),
+                    gain=1.0,
+                )
+                for stage in range(0, n_blocks)
+            ]
+        )
+
+    def forward(self, w, block: int = 0, alpha: float = 0.0):
+        if block > self.n_blocks:
+            raise ValueError(
+                f"This model only has {self.n_blocks} blocks. depth parameter has to be <= n_blocks"
+            )
+        x = self.init_block(w[:, 0:2])
+
+        if block == 0:
+            return self.rgb_converters[0](self.layers[0](x))
+        for layer_block in self.layers[:block]:
+            x = layer_block(x)
+        residual = nn.functional.interpolate(
+            self.rgb_converters[block - 1](x), scale_factor=2
+        )
+        straight = self.rgb_converters[block](self.layers[block](x))
+        return (alpha * straight) + ((1 - alpha) * residual)
+
+
+class Generator(nn.Module):
+    def __init__(
+        self,
+        n_blocks: int = 10,
+        latent_size: int = 512,
+        n_channels: int = 3,
+        style_mixing_prob=0.9,
+    ):
+        super(Generator, self).__init__()
+
+        self.style_mixing_prob = style_mixing_prob
+
+        self.truncation = Truncation(
+            avg_latent=torch.zeros(latent_size),
+            max_layer=8,
+            threshold=0.7,
+            beta=0.995,
+        )
+
+        self.g_mapping = StyleGANMappingNetwork(latent_size, 8)
+        self.g_synthesis = GSynthesis(
+            n_blocks=n_blocks, latent_size=latent_size, n_channels=n_channels
+        )
+
+    def forward(self, x, block: int, alpha: float):
+        w = self.g_mapping(x)
+
+        if self.training:
+            latents2 = torch.randn(x.shape).to(x.device)
+            dlatents2 = self.g_mapping(latents2)
+            layer_idx = torch.from_numpy(
+                np.arange(self.num_layers)[np.newaxis, :, np.newaxis]
+            ).to(x.device)
+            cur_layers = 2 * (block + 1)
+            mixing_cutoff = (
+                random.randint(1, cur_layers)
+                if random.random() < self.style_mixing_prob
+                else cur_layers
+            )
+            w = torch.where(layer_idx < mixing_cutoff, w, dlatents2)
+
+            w = self.truncation(w)
+
+        return self.g_synthesis(w, block, alpha)
